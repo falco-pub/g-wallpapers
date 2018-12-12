8d1520992e9222d4643e29038287340bf2f41293
Falco <falco@falco.in>
2018-12-12 01:21:53 +0100

    fetch the list of photo id's from an album

--- api.py
+++ api.py
@@ -2,6 +2,7 @@
 
 
 # Setup the Photo v1 API
+# from https://github.com/ido-ran/google-photos-api-python-quickstart/blob/master/quickstart.py
 from apiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
@@ -18,32 +19,61 @@ class Photos():
     def albums(self, *args, **kwargs):
         return self.service.albums(*args, **kwargs)
 
-    def album_search(self, title):
+    def mediaItems(self, *args, **kwargs):
+        return self.service.mediaItems(*args, **kwargs)
+
+    def sharedAlbums(self, *args, **kwargs):
+        return self.service.sharedAlbums(*args, **kwargs)
+
+    def album_search(self, title, _tk=None):
         "return id of album which title is 'title'"
-        tk = None;
-        while True:
-            album_id=None
-            results = self.albums().list(
-                pageSize=50, fields="nextPageToken,albums(id,title)",
-                pageToken=tk).execute()
-            try:
-                tk = results['nextPageToken']
-            except KeyError:
-                tk = None
-            try:
-                items = results['albums']
-            except keyError:
-                return None
-            for item in items:
-                if item['title']==title:
-                    return(item['id'])
-            if not tk:
-                return None
+
+        _results = self.albums().list(
+            pageSize=50, fields="nextPageToken,albums(id,title)",
+            pageToken=_tk).execute()
+        if 'albums' in _results:
+            for _i in _results['albums']:
+                if _i['title']==title:
+                    return _i['id']
+        try:
+            return self.album_search(title, _tk=_results['nextPageToken'])
+        except KeyError:
+            return None
+
+    def list_media_album(self, albumId, _tk=None, _r=[]):
+        "return a list of ID's of media in album"
+        _results = self.mediaItems().search(body={
+            'albumId': albumId,
+            'pageToken': _tk}).execute()
+        if 'mediaItems' in _results:
+            for _i in _results['mediaItems']:
+                _r.append(_i['id'])
+        try:
+            return self.list_media_album(albumId, _tk=_results['nextPageToken'], _r=_r)
+        except KeyError:
+            return _r
+
+
+
+
+def session_gphotos(scope='https://www.googleapis.com/auth/photoslibrary.readonly',
+                 storefile='token.json',
+                 credsfile='creds.json'):
+    return Photos(scope, storefile, credsfile)
 
 
 if __name__ == "__main__" and not __doc__:
-    p = Photos(scope='https://www.googleapis.com/auth/photoslibrary.readonly',
-               storefile='token.json',
-               credsfile='creds.json'
-               )
-    print(p.album_search('wallpapers'))
+    s = session_gphotos()
+    ida = s.album_search('wallpapers')
+    l = list_media_album(ida)
+
+
+
+
+
+
+
+
+
+
+
